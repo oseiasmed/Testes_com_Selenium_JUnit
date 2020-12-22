d9bb09ca92d10e06df60a87a6f3423e133402121
Oséias Medeiros <oseiasmed@yahoo.com.br>
2020-12-22 16:05:49 -0300

    Projeto de teste de software utilizando Selenium Web Driver

---
+++ src/main/java/TesteCadastro.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteCadastro {
+	
+	private WebDriver driver;
+	private CampoTreinamentoPage page;
+
+	@Before
+	public void inicializa(){
+		driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		page = new CampoTreinamentoPage(driver);
+	}
+	
+	@After
+	public void finaliza(){
+		driver.quit();
+	}
+
+	@Test
+	public void deveRealizarCadastroComSucesso(){
+		page.setNome("Oséias");
+		page.setSobrenome("Medeiros");
+		page.setSexoMasculino();
+		page.setComidaPizza();
+		page.setEscolaridade("Mestrado");
+		page.setEsporte("Natacao");
+		page.cadastrar();
+		
+		Assert.assertTrue(page.obterResultadoCadastro().startsWith("Cadastrado!"));
+		Assert.assertTrue(page.obterNomeCadastro().endsWith("Oséias"));
+		Assert.assertEquals("Sobrenome: Medeiros", page.obterSobrenomeCadastro());
+		Assert.assertEquals("Sexo: Masculino", page.obterSexoCadastro());
+		Assert.assertEquals("Comida: Pizza", page.obterComidaCadastro());
+		Assert.assertEquals("Escolaridade: mestrado", page.obterEscolaridadeCadastro());
+		Assert.assertEquals("Esportes: Natacao", page.obterEsportesCadastro());
+	}
+}
